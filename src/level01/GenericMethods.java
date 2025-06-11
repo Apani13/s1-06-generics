dca3413a0f729c252cb87f1d6bbf889c823fb06d
Andrea <andreapan98@gmail.com>
2025-06-11 14:05:39 +0200

    feat: implement GenericMethods class with printArgs method

--- src/level01/GenericMethods.java
+++ src/level01/GenericMethods.java
@@ -1,4 +1,9 @@
 package level01;
 
 public class GenericMethods {
+    public static <T, U, V> void printArgs(T arg1, U arg2, V arg3) {
+        System.out.println("Arg1: " + arg1);
+        System.out.println("Arg2: " + arg2);
+        System.out.println("Arg3: " + arg3);
+    }
 }
