6e76845d5a47e32cde132157ea01220c36da8656
Andrea <andreapan98@gmail.com>
2025-06-11 14:54:38 +0200

    feat: modify generic method to accept variable-length argument list

--- src/level02/exercise2/util/GenericMethods.java
+++ src/level02/exercise2/util/GenericMethods.java
@@ -1,4 +1,9 @@
 package level02.exercise2.util;
 
 public class GenericMethods {
+    public static <T> void printArgs(T... args) {
+        for (T arg : args) {
+            System.out.println(arg);
+        }
+    }
 }
