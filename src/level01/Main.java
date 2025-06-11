fb0dca5a17f684845aa10a7db05374b3586b48d9
Andrea <andreapan98@gmail.com>
2025-06-11 14:09:45 +0200

    feat: add Main class with tests for NoGenericMethods andGenericMethods

--- src/level01/Main.java
+++ src/level01/Main.java
@@ -1,5 +1,21 @@
 package level01;
 
 public class Main {
+public static void main(String[] args) {
+    NoGenericMethods noGenericMethods = new NoGenericMethods("first", "second", "third");
 
+    System.out.println("NoGenericMethods args:");
+    System.out.println(noGenericMethods.getArg1());
+    System.out.println(noGenericMethods.getArg2());
+    System.out.println(noGenericMethods.getArg3());
+
+    Person person = new Person("Alma", "Calandraca", 80);
+    String text = "Hello world!";
+    int number = 5545;
+
+    System.out.println("\nGenericMethods printArgs with different types: ");
+    GenericMethods.printArgs(person, text, number);
+
+
+}
 }
